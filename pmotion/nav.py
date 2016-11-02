adc696ca5bdbca719f2c187d032359399205e4e0
nikonikolov <nikonikolov@github.com>
2016-11-02 11:37:10 +0000

    Fix nav.py

--- pmotion/nav.py
+++ pmotion/nav.py
@@ -1,30 +1,34 @@
-import motor_params
+import collections
 import math
 
-def navigateToWaypoint(state, dest):
-	ugoal_theta = atan2((state.y-dest.y)/(state.x-dest.x))
-	delta_theta_rad = state.theta - goal_theta
-	if delta_theta_rad > math.pi:
-		delta_theta_rad = 2*math.pi - delta_theta_rad
-	motor_params.rotate(delta_theta_rad/math.pi*180)
-	state.theta = goal.theta
-	dist = sqrt((state.y - dest.y)**2 + (state.x - dest.x)**2)
-	motor_params.forward(dist)
+import motor_params
 
-	return state
+State = collections.namedtuple("State", ["x", "y", "theta"])
+Dest = collections.namedtuple("Dest", ["x", "y"])
+
+
+def navigateToWaypoint(state, dest):
+    goal_theta = math.atan2(dest.y - state.y, dest.x - state.x)
+    delta_theta_rad = state.theta - goal_theta
+    if delta_theta_rad > math.pi:
+        delta_theta_rad = 2*math.pi - delta_theta_rad
+    motor_params.rotate(delta_theta_rad / math.pi * 180.0)
+    new_theta = goal_theta
+    dist = math.sqrt((state.y - dest.y)**2 + (state.x - dest.x)**2)
+    motor_params.forward(dist)
+
+    return State(x=dest.x, y=dest.y, theta=new_theta)
 
 
 def main():
-	state.x = 0
-	state.y = 0
-	state.theta = 0
-	
-	while true:
-		args = input("Enter a destination waypoint")	
-		dest_list = args.split()
-		dest.x = float(dest_list[0])
-		dest.y = float( dest_list[1])
-		state = navigateToWaypint(state, dest)
+    state = State(x=0, y=0, theta=0)
+    
+    while True:
+        args = raw_input("Enter a destination waypoint")
+        x, y = args.strip().split()
+        dest = Dest(x=float(x), y=float(y))
+        state = navigateToWaypoint(state, dest)
 
-	
-	
+
+if __name__ == "__main__":
+    main()
