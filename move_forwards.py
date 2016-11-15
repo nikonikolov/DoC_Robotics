49dc1d9e68a6d41b8e3f9196ca7e6810c5bf421c
nikonikolov <nikonikolov@github.com>
2016-11-15 22:33:12 +0000

    oh

--- move_forwards.py
+++ move_forwards.py
@@ -1,6 +1,7 @@
 import motor_params
 
-input = float(raw_input("Enter distance: "))
+#input = float(raw_input("Enter distance: "))
 
-motor_params.forward(input)
+
+motor_params.forward(100)
 #motor_params.forward(-100)
