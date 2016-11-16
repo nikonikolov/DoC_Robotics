be096890405e79ad3f276e7707b9069bf62a2278
nikonikolov <nikonikolov@github.com>
2016-11-16 11:39:09 +0000

    Some tuning

--- motor_params.py
+++ motor_params.py
@@ -20,17 +20,17 @@ motorParamsLeft = interface.MotorAngleControllerParameters()
 motorParamsLeft.maxRotationAcceleration = 8.0
 motorParamsLeft.maxRotationSpeed = 12.0
 motorParamsRight = interface.MotorAngleControllerParameters()
-motorParamsRight.maxRotationAcceleration = 8.6
-motorParamsRight.maxRotationSpeed = 12.1
+motorParamsRight.maxRotationAcceleration = 8.0
+motorParamsRight.maxRotationSpeed = 12.0
 # tune all the following parameters
 
 motorParamsLeft.feedForwardGain = 255/20.0
 motorParamsLeft.minPWM = 18.0
 motorParamsLeft.pidParameters.minOutput = -255
 motorParamsLeft.pidParameters.maxOutput = 255
-motorParamsLeft.pidParameters.k_p = 0.6*G
+motorParamsLeft.pidParameters.k_p = .65*G
 #motorParamsLeft.pidParameters.k_i = 2*motorParamsLeft.pidParameters.k_p/T*0.3
-motorParamsLeft.pidParameters.k_i = 2*motorParamsLeft.pidParameters.k_p/T*0.3
+motorParamsLeft.pidParameters.k_i = 1.7*motorParamsLeft.pidParameters.k_p/T*0.3
 motorParamsLeft.pidParameters.K_d = motorParamsLeft.pidParameters.k_p*T/8
 
 motorParamsRight.feedForwardGain = 255/20.0
@@ -39,7 +39,7 @@ motorParamsRight.pidParameters.minOutput = -255
 motorParamsRight.pidParameters.maxOutput = 255
 motorParamsRight.pidParameters.k_p = 0.6*G
 #motorParamsRight.pidParameters.k_i = 2*motorParamsRight.pidParameters.k_p/T*0.3
-motorParamsRight.pidParameters.k_i = 2*motorParamsRight.pidParameters.k_p/T*0.3
+motorParamsRight.pidParameters.k_i = 0.5*motorParamsRight.pidParameters.k_p/T*0.3
 motorParamsRight.pidParameters.K_d = motorParamsRight.pidParameters.k_p*T/8
 
 interface.setMotorAngleControllerParameters(motors[MOTOR_LEFT], motorParamsLeft)
--- move_forwards.py
+++ move_forwards.py
@@ -3,5 +3,5 @@ import motor_params
 #input = float(raw_input("Enter distance: "))
 
 
-motor_params.forward(50)
+motor_params.forward(100)
 #motor_params.forward(-100)
