5753450e3563d64e12c61019d395b94f96cae82b
nikonikolov <niko.b.nikolov@gmail.com>
2016-11-29 15:52:35 +0000

    threshold fix

--- place_rec/place_rec.py
+++ place_rec/place_rec.py
@@ -30,11 +30,7 @@ else:
 
 # NOTE: if you change this, reading a signature from file might read a wrong signature; Comparing signatures will also fail; Solution - delete all current signatures
 STEP = 5
-
-FACE_FORWARD = math.pi / 2
-FACE_LEFT = math.pi
-FACE_RIGHT = 0.0
-FACE_BACK = -math.pi / 2
+THRESHOLD_DIFFERENCE = 260.0
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 NORMAL_DIR = SCRIPT_DIR + "/data/normal/"
@@ -97,8 +93,7 @@ def remove_dc_component(x):
 
 def threshold_differences(test_values, observations):
     # Then only we threshold them.
-    threshold = 500.0
-    return [1 if ((observed - expected) ** 2) > 200.0 else 0
+    return [1 if ((observed - expected) ** 2) > THRESHOLD_DIFFERENCE else 0
             for observed, expected
             in zip(observations, test_values)]
 
@@ -155,7 +150,7 @@ def get_bottle_belief(test_signature, observed_signature, point):
     print filtered_test_sig
     print "Angles"
     print angles
-
+he
     # Get a list of ranges speficying the stard and end indices of the biggest clusters 
     clusters = remove_cluster_anomalies(binary_signal_partition_by(thresholded))
     if len(clusters) == 1:
